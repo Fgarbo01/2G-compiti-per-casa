d10e7d3fa49aa779a71a710e3b43efcea7a0f2c4
Fgarbo01 <fgarbo01@gmail.com>
2017-04-21 22:22:38 +0200

    Add files via upload

---
+++ char.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    char min=-128;
+    char max=127;
+    cout<<"ecco il valore minimo:"<<(int)min<<endl;
+    cout<<"ecco il minimo-1:"<<min-1<<endl;
+    cout<<"ecco il valore massimo:"<<(int)max<<endl;
+    cout<<"ecco il massimo+1:"<<max+1<<endl;
+    return 0;
+}
