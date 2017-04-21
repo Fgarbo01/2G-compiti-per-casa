3ce64eaa4275ea16267949aed4c1296e44f3042f
Fgarbo01 <fgarbo01@gmail.com>
2017-04-21 22:22:19 +0200

    Add files via upload

---
+++ unsigned.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+
+int main()
+{
+    std::unsigned int max=4294967295;
+    std::unsigned int min=0;
+    std::cout<<"ecco il minimo:"<<min<<endl;
+    std::cout<<"ecco il massimo:"<<max<<endl;
+    std::cout<<"ecco il minimo-1:"<<min-1<<endl;
+    std::cout<<"ecco il massimo+1:"<<max+1<<endl;
+    return 0;
+}
